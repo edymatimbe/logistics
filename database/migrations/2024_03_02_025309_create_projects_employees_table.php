b828c6e8acabcea08897ff3b321f7f1e1c058210
edymatimbe <58170972+edymatimbe@users.noreply.github.com>
2024-03-02 08:44:14 +0200

    first commit

---
+++ database/migrations/2024_03_02_025309_create_projects_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects_employees', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained(
+                table: 'employees',
+                column: 'id',
+                indexName: 'employees_pe_fk'
+            );
+            $table->foreignId('project_id')->constrained(
+                table: 'projects',
+                column: 'id',
+                indexName: 'projects_pe_fk'
+            );
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects_employees');
+    }
+};
